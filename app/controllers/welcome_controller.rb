c37bc7fa1e708f4f96e834c874d3f6dc29301713
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-02-03 18:47:36 +0530

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+    def index
+        @title = "my title"
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'c62135ee12db939a214dfea2996a5452a229395789e6c581178801b70a211050c24f783c4b45ef496347011388076a9c82f5e42c6302e7af6a60317fba47ad25'
